d17cd498a60c7821361aef2b9cfd2b84bfa1074e
WanHsuanLin <b06901054@ntu.edu.tw>
2022-03-31 17:26:58 -0700

    upd write csv

--- qArchSearc/writeCSV.py
+++ qArchSearc/writeCSV.py
@@ -11,38 +11,37 @@ def create_list_from_json(jsonfile:str):
     data_list = []  # create an empty list
 
     # append the items to the list in the same order.
-    data_list.append(data.get('device'))
     data_list.append(data.get('M'))
     data_list.append(data.get('D'))
     data_list.append(data.get('g1'))
     data_list.append(data.get('g2'))
-    if args.benchmark == "qaoa":
-        data_list.append(data.get('trial'))
-    if args.ifcrosstalk:
-        data_list.append(data.get('crosstalk'))
+    data_list.append(data.get('extra_edge_num'))
+    data_list.append(data.get('extra_edge'))
+    data_list.append(data.get('cost'))
+    data_list.append(data.get('fidelity'))
+    data_list.append(data.get('cost-scaled fidelity'))
+    data_list.append(data.get('crosstalk'))
+    data_list.append(data.get('fidelity_ct'))
+    data_list.append(data.get('cost-scaled fidelity_ct'))
+    
     return data_list
 
 def write_csv():
     list_of_files = get_list_of_json_files(args.folder)
-    # if len(list_of_files) < 100:
-    #     return
     tmpStr = args.folder.split('/')
-    # csvName = tmpStr[-3] + '/' + tmpStr[-2] + '/csv/' + tmpStr[-1] + '_f.csv'
-    # csvName = tmpStr[-4] + '/' + tmpStr[-3] + '/' + tmpStr[-2] + '/csv/' + tmpStr[-1] + '_f.csv'
-    csvName = tmpStr[-5] + '/' + tmpStr[-4] + '/' + tmpStr[-3] + '/' + tmpStr[-2] + '/csv/' + tmpStr[-1] + '_f.csv'
-    # csvName = tmpStr[0] + '/csv/' + tmpStr[1] + '_f.csv'
+    csv_name = '_f.csv'
+    for i, s in enumerate(reversed(tmpStr)):
+        if i == 0:
+            csv_name = s + csv_name
+        if i == 1:
+            csv_name = s + '/csv/' + csv_name
+        else:
+            csv_name = s + '/' + csv_name
+
     with open(csvName, 'w+') as c:
         writer = csv.writer(c)
-        if args.benchmark == "qaoa":
-            if args.ifcrosstalk:
-                writer.writerow(["device", "M", "D", "g1", "g2", "trial", "crosstalk"])
-            else:
-                writer.writerow(["device", "M", "D", "g1", "g2", "trial"])
-        else:
-            if args.ifcrosstalk:
-                writer.writerow(["device", "M", "D", "g1", "g2", "crosstalk"])
-            else:
-                writer.writerow(["device", "M", "D", "g1", "g2"])
+        writer.writerow(['M', 'D', 'g1', 'g2', 'extra_edge_num', \
+        'extra_edge', 'cost', 'fidelity', 'cost-scaled fidelity', 'crosstalk', 'fidelity_ct', 'cost-scaled fidelity_ct']
     c.close()
     for file in list_of_files:
         row = create_list_from_json(file)  # create the row to be added to csv for each file (json-file)
@@ -54,19 +53,9 @@ def write_csv():
     cal_fid_dec(df)
     cal_crosstalk_ratio(df)
 
-    sorted_df = df.sort_values(by=['device'])  
-
-    if args.benchmark == "qaoa":
-        sorted_df = sorted_df.groupby('device').agg([np.mean])
-        sorted_df = sorted_df.rename_axis('device').reset_index()
-        sorted_df = sorted_df.columns.to_frame().T.append(sorted_df, ignore_index=True)
-        sorted_df.columns = range(len(sorted_df.columns))
-        sorted_df = sorted_df.drop(1)
-        sorted_df.columns = sorted_df.iloc[0]
-        sorted_df = sorted_df.drop(0)
-        sorted_df = sorted_df.drop(columns='trial')
+    sorted_df = df.sort_values(by=['extra_edge_num'])  
     
-        cal_improve_ratio(sorted_df, tmpStr[-1])
+    cal_improve_ratio(sorted_df)
     sorted_df = sorted_df.reset_index(drop=True)
 
     sorted_df.to_csv(csvName, index=False)
@@ -77,10 +66,9 @@ def cal_fid_dec(df):
 def cal_crosstalk_ratio(df):
     df['ct_ratio'] = df['crosstalk'] / df['g2']
 
-def cal_improve_ratio(df, case):
+def cal_improve_ratio(df):
     baseLine = df.iloc[0]['fidelity']
     df['f_improve_r'] = (df['fidelity']-baseLine) / baseLine
-    # df['cost-scaled fidelity'] = (100 * df['f_improve_r']) / df['cost']
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -89,12 +77,6 @@ if __name__ == "__main__":
         help="Result Folder: each benchmark result")
     parser.add_argument("benchmark", metavar='B', type=str,
         help="Benchmark Set: arith or qaoa or QCNN")
-    parser.add_argument("--device", dest="ifdevice", action='store_true',
-        help="if you want to draw curve sorted by the index of device")
-    parser.add_argument("--crosstalk", dest="ifcrosstalk", action='store_true',
-        help="if you want to consider crosstalk effect")
-    parser.add_argument("--comment", dest="comment", type=str,
-        help="if you want to comment this run in any way")
     # Read arguments from command line
     args = parser.parse_args()
     write_csv()
--- runArchSearch.py
+++ runArchSearch.py
@@ -8,56 +8,57 @@ from qArchSearc.device import get_device_set
 from qArchSearc.util import get_qaoa_graph
 #from memory_profiler import memory_usage
 
-# Initialize parser
-parser = argparse.ArgumentParser()
-# Adding optional argument
-parser.add_argument("benchmark", metavar='B', type=str,
-    help="Benchmark Set: arith or qaoa or qcnn or qv")
-parser.add_argument("folder", metavar='F', type=str,
-    help="the folder to store results")
-parser.add_argument("--size", dest="size", type=int,
-    help="The size of the qaoa circuit: 8, 10, 12, 14, 16")
-parser.add_argument("--trial", dest="trial", type=int,
-    help="The index of qaoa circuit: from 0 to 9")
-parser.add_argument("--filename", dest="filename", type=str,
-    help="The file name of the arith circuit")
-parser.add_argument("--tran", dest="iftran", action='store_true',
-    help="if you want to use TB-OLSQ rather than OLSQ")
-parser.add_argument("--memory_max_size", dest="memory_max_size", type=int, default=0,
-    help="set hard upper limit for memory consumption (G)")
-parser.add_argument("--verbose", dest="verbose", type=int, default=0,
-    help="verbose level for Z3")
-args = parser.parse_args()
+if __name__ == "__main__":
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    # Adding optional argument
+    parser.add_argument("benchmark", metavar='B', type=str,
+        help="Benchmark Set: arith or qaoa or qcnn or qv")
+    parser.add_argument("folder", metavar='F', type=str,
+        help="the folder to store results")
+    parser.add_argument("--size", dest="size", type=int,
+        help="The size of the qaoa circuit: 8, 10, 12, 14, 16")
+    parser.add_argument("--trial", dest="trial", type=int,
+        help="The index of qaoa circuit: from 0 to 9")
+    parser.add_argument("--filename", dest="filename", type=str,
+        help="The file name of the arith circuit")
+    parser.add_argument("--tran", dest="iftran", action='store_true',
+        help="if you want to use TB-OLSQ rather than OLSQ")
+    parser.add_argument("--memory_max_size", dest="memory_max_size", type=int, default=0,
+        help="set hard upper limit for memory consumption (G)")
+    parser.add_argument("--verbose", dest="verbose", type=int, default=0,
+        help="verbose level for Z3")
+    args = parser.parse_args()
 
-# defulat using TB-OLSQ 
-my_mode = "normal"
-if args.iftran:
-    my_mode = "transition"
+    # defulat using TB-OLSQ 
+    my_mode = "normal"
+    if args.iftran:
+        my_mode = "transition"
 
-lsqc_searcher = qArchEval(mode=my_mode)
+    lsqc_searcher = qArchEval(mode=my_mode)
 
-lsqc_searcher.setdevice(get_device_set(benchmark=args.benchmark))
+    lsqc_searcher.setdevice(get_device_set(benchmark=args.benchmark))
 
-if args.benchmark == "qaoa":
-    program = [args.size,
-        get_qaoa_graph(size=args.size, trial=args.trial),
-        ["ZZ" for _ in range( (args.size * 3) // 2 )] ]
-    lsqc_searcher.setprogram(args.benchmark, program, "IR")
-else:
-    file = open(args.filename)
-    lsqc_searcher.setprogram(args.benchmark, file.read())
-    file.close()
+    if args.benchmark == "qaoa":
+        program = [args.size,
+            get_qaoa_graph(size=args.size, trial=args.trial),
+            ["ZZ" for _ in range( (args.size * 3) // 2 )] ]
+        lsqc_searcher.setprogram(args.benchmark, program, "IR")
+    else:
+        file = open(args.filename)
+        lsqc_searcher.setprogram(args.benchmark, file.read())
+        file.close()
 
-#mem_usage = memory_usage(f)
-print("start searching...")
-results = lsqc_searcher.search(memory_max_size=args.memory_max_size*1000, verbose=args.verbose)
+    #mem_usage = memory_usage(f)
+    print("start searching...")
+    results = lsqc_searcher.search(memory_max_size=args.memory_max_size*1000, verbose=args.verbose)
 
-for i, result in enumerate(results):
-    with open(f"./{args.folder}/extra_edge_{i}.json", 'w') as file_object:
-        if args.benchmark == "qcnn":
-            result["size"] = args.filename.split('_')[0]
-        elif argsbenchmark == "arith":
-            result["file"] = args.filename.split('.')[0]
-        else:
-            result["size"] = args.size
-        json.dump(result, file_object)
+    for i, result in enumerate(results):
+        with open(f"./{args.folder}/extra_edge_{i}.json", 'w') as file_object:
+            if args.benchmark == "qcnn":
+                result["size"] = args.filename.split('_')[0]
+            elif argsbenchmark == "arith":
+                result["file"] = args.filename.split('.')[0]
+            else:
+                result["size"] = args.size
+            json.dump(result, file_object)
