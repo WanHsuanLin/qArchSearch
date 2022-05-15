deeaaa902bc50a64b813c4a034fdabc05565f53c
WanHsuanLin <b06901054@ntu.edu.tw>
2022-05-14 18:12:21 -0700

    upd results

---
+++ tmp_exp2.sh
@@ -0,0 +1,8 @@
+./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 6
+./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 6
+./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 7
+./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 7  
+./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 8
+./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 8
+./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 9
+./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 9  
