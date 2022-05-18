24c2836dee2708ef7ed39b5adbf1f0488fc63fa3
WanHsuanLin <b06901054@ntu.edu.tw>
2022-05-18 11:08:26 -0700

    upd results

--- tmp_exp2.sh
+++ tmp_exp2.sh
@@ -1,3 +1,4 @@
+./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 2
 ./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 6
 ./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 6
 ./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 7
