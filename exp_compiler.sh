f291a62e5473722f63d9e32403304be6f260da5b
WanHsuanLin <b06901054@ntu.edu.tw>
2022-06-09 22:02:19 -0700

    upd gate dur

--- exp_compiler.sh
+++ exp_compiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ./exp_compiler.sh h hh device_set/hh/qcnn/8-4-2_gs_d.json qcnn qcnn/8-4-2.qasm
-# ./exp_compiler.sh h grid device_set/hh/qcnn/8-4-2_gs_d.json qcnn qcnn/8-4-2.qasm
+# ./exp_compiler.sh o grid device_set/hh/qcnn/8-4-2_gs_d.json qcnn qcnn/8-4-2.qasm
 # ./exp_compiler.sh h hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 0
 # ./exp_compiler.sh h grid device_set/hh/qaoa/8_0_gs_d.json qaoa 8 0
 # ./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 0
