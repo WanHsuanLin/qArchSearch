9f683307be14aa92a7d444af935d9b81f3829910
WanHsuanLin <b06901054@ntu.edu.tw>
2022-07-05 22:29:37 -0700

    upd script

--- exp_cross_design.sh
+++ exp_cross_design.sh
@@ -18,7 +18,7 @@
 # ./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 14
 # ./exp_compiler.sh o hh device_set/hh/qaoa/8_0_gs_d.json qaoa 8 15
 # ./exp_compiler.sh o grid device_set/grid/qaoa/8_0_gs_d.json qaoa 8 15
-N = 4
+N=4
 
 for i in 2 5 7 8 10; do
     (   
